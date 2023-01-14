3d51a3c36881b34bfc7236bf0de9fc9fe0d14dc7
SofiLovelace <kaozztrs@gmail.com>
2023-01-13 19:45:41 -0600

    ajustes a main e index

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,24 @@
 // aqui exportaras las funciones que necesites
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
+// Este es el punto de entrada de tu aplicacion
+// Import the functions you need from the SDKs you need
+
+// const analytics = getAnalytics(app);
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDsV641BeYPAlGnaSM_CPuV5nWGVNOiPZs',
+  authDomain: 'catslover012023.firebaseapp.com',
+  databaseURL: 'https://catslover012023-default-rtdb.firebaseio.com',
+  projectId: 'catslover012023',
+  storageBucket: 'catslover012023.appspot.com',
+  messagingSenderId: '262132831591',
+  appId: '1:262132831591:web:12a097d59110e034244bc2',
+  measurementId: 'G-K4QCS1CZVP',
 };
+
+export default firebaseConfig;
