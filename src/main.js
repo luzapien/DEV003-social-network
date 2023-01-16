8e80e7dd664c3a0064d1e9f6232f1dd5d383baba
María Rodríguez <lu.rzapien.92@gmail.com>
2023-01-16 16:07:21 -0600

    Cambios para registro de usuario

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import {
   getAuth,
   GoogleAuthProvider,
   signInWithPopup,
+  createUserWithEmailAndPassword,
 } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
-import { myFunction } from './lib/index.js';
+// import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js';
+import firebaseConfig from './lib/index';
 
+// import { getAnalytics } from 'firebase/analytics';
 // const analytics = getAnalytics(app);
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -16,44 +18,46 @@ import { myFunction } from './lib/index.js';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyDsV641BeYPAlGnaSM_CPuV5nWGVNOiPZs',
-  authDomain: 'catslover012023.firebaseapp.com',
-  databaseURL: 'https://catslover012023-default-rtdb.firebaseio.com',
-  projectId: 'catslover012023',
-  storageBucket: 'catslover012023.appspot.com',
-  messagingSenderId: '262132831591',
-  appId: '1:262132831591:web:12a097d59110e034244bc2',
-  measurementId: 'G-K4QCS1CZVP',
-};
+
 const btnGoogle = document.getElementById('btnGoogle');
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-console.log(app);
+// const db = getFirestore();
+// console.log(app);
 
-btnGoogle.addEventListener('click', () => {
+btnGoogle.addEventListener('click', async () => {
   const auth = getAuth();
   const provider = new GoogleAuthProvider();
-  signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      console.log(user);
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      console.log(error);
-    });
+  try {
+    const result = await signInWithPopup(auth, provider);
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user);
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    console.log(error);
+  }
 });
 
-const auth = getAuth(app);
-
-myFunction();
+const btnRegister = document.getElementById('btnRegister');
+btnRegister.addEventListener('click', async () => {
+  const auth = getAuth(app);
+  const email = document.getElementById('txtMail').value;
+  const password = document.getElementById('txtPass').value;
+  console.log(email, password);
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    console.log(userCredential);
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(error);
+  }
+});
