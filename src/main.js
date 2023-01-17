65fbb49ad97936ed3f070b7e55652d90f96d280c
Chris <chris.olivosf@gmail.com>
2023-01-16 19:56:01 -0500

    login funcionando

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import {
   GoogleAuthProvider,
   signInWithPopup,
   createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 // import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js';
+// import { async } from 'regenerator-runtime';
 import firebaseConfig from './lib/index';
+// import { async } from 'regenerator-runtime';
 
 // import { getAnalytics } from 'firebase/analytics';
 // const analytics = getAnalytics(app);
@@ -21,6 +26,17 @@ const btnGoogle = document.getElementById('btnGoogle');
 const app = initializeApp(firebaseConfig);
 // const db = getFirestore();
 // console.log(app);
+const auth = getAuth();
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    document.getElementById('scWelcome').style.display = 'block';
+    document.getElementById('scAccess').style.display = 'none';
+    console.log(user);
+  } else {
+    document.getElementById('scWelcome').style.display = 'none';
+    document.getElementById('scAccess').style.display = 'block';
+  }
+});
 
 btnGoogle.addEventListener('click', async () => {
   const auth = getAuth();
@@ -31,7 +47,7 @@ btnGoogle.addEventListener('click', async () => {
     const token = credential.accessToken;
     // The signed-in user info.
     const user = result.user;
-    console.log(user);
+    // console.log(user);
   } catch (error) {
     const errorCode = error.code;
     const errorMessage = error.message;
@@ -39,22 +55,67 @@ btnGoogle.addEventListener('click', async () => {
     const email = error.customData.email;
     // The AuthCredential type that was used.
     const credential = GoogleAuthProvider.credentialFromError(error);
-    console.log(error);
+    // console.log(error);
   }
 });
 
 const btnRegister = document.getElementById('btnRegister');
 btnRegister.addEventListener('click', async () => {
+  // llamar funcion validar correo nuevo (correo, email)
   const auth = getAuth(app);
   const email = document.getElementById('txtMail').value;
   const password = document.getElementById('txtPass').value;
-  console.log(email, password);
+  //  console.log(email, password);
   try {
     const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    console.log(userCredential);
+    document.getElementById('scWelcome').style.display = 'block';
+    document.getElementById('scAccess').style.display = 'none';
+    // console.log(userCredential);
   } catch (error) {
     const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(error);
+    //  const errorMessage = error.message;
+
+    console.log(errorCode);
+    // auth/invalid-email
+
+    if (error.code === 'auth/email-already-in-use') {
+      alert('Ya hay un usuario registrado con el correo');
+      // falta limpiar el correo y usuario
+    } else if (error.code === 'auth/internal-error' || error.code === 'auth/invalid-email') {
+      alert('Ingrese un correo valido');
+    } else if (error.code === 'auth/weak-password') {
+      alert('La contraseña debe tener minimo 6 caracteres');
+    }
+  }
+});
+
+const logout = document.getElementById('btnLogout');
+logout.addEventListener('click', async () => {
+  await signOut(auth);
+  document.getElementById('scWelcome').style.display = 'none';
+  document.getElementById('scAccess').style.display = 'block';
+  console.log('sesion cerrada');
+});
+
+// btnLogin
+const loginWithMail = document.getElementById('btnLogin');
+loginWithMail.addEventListener('click', async () => {
+  try {
+    const auth = getAuth(app);
+    const email = document.getElementById('txtMail').value;
+    const password = document.getElementById('txtPass').value;
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    document.getElementById('scWelcome').style.display = 'block';
+    // alert(`Bienvenido ${auth}`)  ;
+    alert(`Bienvenido${email}`);
+    document.getElementById('scAccess').style.display = 'none';
+  } catch (error) {
+    console.log(error.code);
+    if (error.code === 'auth/wrong-password') {
+      alert('Contraseña incorrecta');
+    } else if (error.code === 'auth/user-not-found') {
+      alert('Correo no registrado');
+    }
+    // auth/wrong-password
   }
 });
