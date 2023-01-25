e5520d0281363c7ffd2980946914d5559b1a0f9c
SofiLovelace <kaozztrs@gmail.com>
2023-01-24 18:18:52 -0600

    probando tests

--- src/lib/__mock__/firebase.js
+++ src/lib/__mock__/firebase.js
@@ -1,2 +1,8 @@
-jest.mockService('../../components/Login.js');
+// jest.mock('firebase/auth');
+import { emailLogin } from '../firebase';
 
+export const resultEmailLogin = async () => {
+  const resultPromise = await emailLogin('catslover@gmail.com', 123456);
+  console.log(resultPromise);
+  return resultPromise;
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,7 @@
 // importamos la funcion que vamos a testear
 // import { myFunction } from '../src/lib/index';
 // import { isValidTimestamp } from '@firebase/util';
-import { /* loginWithGoogle, */ registerNewUser } from '../src/lib/firebase';
+// import { /* loginWithGoogle, */ registerNewUser } from '../src/lib/firebase';
 
 // describe('myFunction', () => {
 //   it('debería ser una función', () => {
@@ -14,14 +14,25 @@ import { /* loginWithGoogle, */ registerNewUser } from '../src/lib/firebase';
 //     expect(result.length).toBe(1);
 //   });
 // });
-describe('registerNewUser', () => {
-  it('return an user', () => {
-    // const name = 'Chris';
-    // const lastName = 'Olivos';
-    const email = 'test@gmail.com';
-    const password = '123456';
-    // const confirmPass = '123456';
-    const result = registerNewUser(email, password);
-    expect(result.email).toBe('test@gmail.com);
-  });
+// describe('registerNewUser', () => {
+//   it('return an user', () => {
+//     // const name = 'Chris';
+//     // const lastName = 'Olivos';
+//     const email = 'test@gmail.com';
+//     const password = '123456';
+//     // const confirmPass = '123456';
+//     const result = registerNewUser(email, password);
+//     expect(result.email).toBe('test@gmail.com);
+//   });
+// });
+//import { resultEmailLogin } from '../src/lib/__mock__/firebase';
+import { emailLogin } from '../src/lib/firebase';
+
+// jest.mock('firebase/auth');
+jest.mock('../src/lib/firebase.js');
+
+test('should return an email', async () => {
+  const email = await emailLogin('catsLover@gmail.com', 123456);
+
+  expect(email.email).toBe('catslover@gmail.com');
 });
