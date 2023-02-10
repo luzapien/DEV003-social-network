0b410c79c873b8506b462669fb58d99fbde06193
María Rodríguez <lu.rzapien.92@gmail.com>
2023-02-10 12:28:10 -0600

    Resultados de test de usabilidad aplicado

--- src/components/ModalError.js
+++ src/components/ModalError.js
@@ -12,3 +12,19 @@ export function modalError(messageError) {
   });
   return ventanaModal;
 }
+
+export function modalDeletePost(messageError) {
+  const windowModalDelete = document.createElement('div');
+  windowModalDelete.innerHTML = '';
+  windowModalDelete.id = 'windowModalDelete';
+  windowModalDelete.className = 'modal';
+  windowModalDelete.innerHTML = `<div class="modal-content">
+  <span class="cerrar">&times;</span>
+  <h2>Verifica el siguiente dato:</h2>
+  <p id = 'textModalError' class = 'textError'>${messageError}</p>
+  <button id = 'yes'>Si</button>
+  <button id = 'cancel'>Cancelar</button>
+</div>`;
+
+  return windowModalDelete;
+}
