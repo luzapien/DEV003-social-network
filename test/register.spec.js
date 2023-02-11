d9c974ce80087f632ecc75132e9f526c19895193
SofiLovelace <kaozztrs@gmail.com>
2023-02-10 19:27:02 -0600

    tests de register

--- test/register.spec.js
+++ test/register.spec.js
@@ -18,8 +18,6 @@ function tick() {
   });
 }
 
-const users = ['kaozztrs@gmail.com', 'chris@gmail.com', 'luzapien@gmail.com'];
-
 describe('Registro con contraseñas diferentes en imputs', () => {
   let inputName;
   let inputLastname;
@@ -52,28 +50,21 @@ describe('Registro con contraseñas diferentes en imputs', () => {
 });
 
 describe('registro con un correo ya registrado', () => {
-  let viewContainer;
-  let form;
   let inputName;
   let inputLastname;
   let inputEmail;
   let inputPassword;
   let inputConfirmPassword;
   let buttonRegister;
-  let buttonReturnLogin;
-  let textModalError;
 
   beforeEach(() => {
     document.body.appendChild(Register());
-    viewContainer = document.getElementById('containerRegister');
-    form = document.getElementById('formRegister');
     inputName = document.getElementById('nameId');
     inputLastname = document.getElementById('lastnameId');
     inputEmail = document.getElementById('emailId');
     inputPassword = document.getElementById('passwordId');
     inputConfirmPassword = document.getElementById('confirmPasswordId');
     buttonRegister = document.getElementById('buttonRegisterHome');
-    buttonReturnLogin = document.getElementById('btn-return-login');
   });
 
   it('Debería mostrar un error', async () => {
@@ -89,8 +80,41 @@ describe('registro con un correo ya registrado', () => {
     buttonRegister.click();
     await tick();
     const textErrorModal2 = document.getElementById('textModalError');
-    console.log('prueba 2 =========>', textErrorModal2);
-    console.log('prueba 2 =========>', textErrorModal2.textContent);
     expect(textErrorModal2.textContent).toBe('Ya hay un usuario registrado con el correo');
+  });
+});
+
+describe('registro con una contraseña debil', () => {
+  let inputName;
+  let inputLastname;
+  let inputEmail;
+  let inputPassword;
+  let inputConfirmPassword;
+  let buttonRegister;
+
+  beforeEach(() => {
+    document.body.appendChild(Register());
+    inputName = document.getElementById('nameId');
+    inputLastname = document.getElementById('lastnameId');
+    inputEmail = document.getElementById('emailId');
+    inputPassword = document.getElementById('passwordId');
+    inputConfirmPassword = document.getElementById('confirmPasswordId');
+    buttonRegister = document.getElementById('buttonRegisterHome');
+  });
+
+  it('Debería mostrar un error', async () => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    registerNewUser.mockImplementationOnce(() => Promise.reject({ code: 'auth/weak-password' }));
+    const windowModal = document.getElementById('textErrorModal');
+    windowModal.click();
+    inputName.value = 'Chris';
+    inputLastname.value = 'Olivos';
+    inputEmail.value = 'chris@gmail.com';
+    inputPassword.value = '12';
+    inputConfirmPassword.value = '12';
+    buttonRegister.click();
+    await tick();
+    const textErrorModal2 = document.getElementById('textModalError');
+    expect(textErrorModal2.textContent).toBe('La contraseña debe tener minimo 6 caracteres');
   });
 });
